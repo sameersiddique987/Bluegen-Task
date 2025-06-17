e81264fb93a9008be03853a3409cd1f27d0b3eab
sameersiddique987 <sameersiddique301@gmail.com>
2025-06-16 22:46:11 -0700

    Updated

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL:"http://localhost:5000" ,
+  baseURL:"https://bluegen-backend.vercel.app" ,
 });
 
 API.interceptors.request.use((config) => {
